97433ed3873582107a361a0923961b6a432d32e1
ar20190114 <skdr0724@gmail.com>
2022-12-04 12:35:44 +0900

    modelの変更

--- api_app/models.py
+++ api_app/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from uuid import uuid4
 
 
 class RecipesInfo(models.Model):
-    id = models.PositiveIntegerField('ID', primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid4, unique=True)
     title = models.CharField('料理名', max_length=100)
     making_time = models.CharField('作成時間', max_length=100)
     serves = models.CharField('人数', max_length=100)
